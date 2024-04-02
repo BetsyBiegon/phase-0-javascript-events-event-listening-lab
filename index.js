b45babf649d90a4879b1b796aca74307654fe0e2
BetsyBiegon <betsybiegon@gmail.com>
2024-04-02 14:07:46 +0300

    add changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    // Event listener setup
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
